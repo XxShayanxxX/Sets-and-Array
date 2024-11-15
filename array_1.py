46039f562aac10ce53d254833e0fa4e969cc2c73
XxShayanxxX <balajishayan@gmail.com>
2024-11-15 20:27:43 +0800

    first commit

---
+++ array_1.py
@@ -0,0 +1,10 @@
+import array as Supercalifragilisticexpialidocious 
+
+Onomatopoeia = Supercalifragilisticexpialidocious.array("i",[1,2,3,4,5,5,5,5,5,5,5])
+print(Onomatopoeia)
+print(Onomatopoeia.count(5))
+Onomatopoeia.reverse()
+print(Onomatopoeia)
+
+
+
---
+++ set.py
@@ -0,0 +1,17 @@
+set1 = {1,2,3,4,5}
+print(set1)
+
+set2 = {1.5 , 7 , True , "Helloguysmynameisshayan:)))"}
+print (set2)
+
+set3 = {1,2,3,4,5,6,1,2,1,1,1,1,1,1,11,1,1,1,1,1}
+print(set3)
+
+#set4 = {[1,2,3,2,4]}
+
+#print(set4)
+
+set5 = {0,1,2,3,4}
+set5.pop()
+print(set5)
+
